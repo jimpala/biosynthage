fd8c12571e9cc150d83330061371eb29dce8db83
Liam <kingliam28@ntlworld.com>
2016-07-09 21:30:50 +0100

    Install, import react-router

--- src/script.js
+++ src/script.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {Router, Route} from 'react-router';
+
 import App from './components/App.jsx';
 
 ReactDOM.render(
-  <App/>,
+  <Router>
+    <Route path="/" component={App}>
+    </Route>
+  </Router>,
   document.getElementById('react-container')
 )
