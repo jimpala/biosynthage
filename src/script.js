21f7d58378950b86c368ae0430cfb124851b2988
Liam <kingliam28@ntlworld.com>
2016-07-09 21:43:53 +0100

    Add a description route + component

--- src/script.js
+++ src/script.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {Router, Route} from 'react-router';
+import { Router, Route } from 'react-router'
+
 
 import App from './components/App.jsx';
+import DescriptionPage from './components/DescriptionPage.jsx';
 
 ReactDOM.render(
   <Router>
-    <Route path="/" component={App}>
-    </Route>
+    <Route path="/" component={App}></Route>
+    <Route path="/description" component={DescriptionPage}></Route>
   </Router>,
   document.getElementById('react-container')
 )
