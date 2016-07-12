c8dcf80ef4aaf127d831e5c6cc70d41327058475
Liam <kingliam28@ntlworld.com>
2016-07-12 17:43:43 +0100

    Video fade in animation

--- src/components/LandingPageInformation.jsx
+++ src/components/LandingPageInformation.jsx
@@ -1,21 +1,44 @@
 import React from 'react';
 
 const styles = {
-  h1: {
-    color: '#000000',
+  div: {
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100vw',
+    height: '100vh',
+  },
+  videoDiv: {
+    position: 'fixed',
+    backgroundColor: 'rgba(0,0,0,1)',
+    opacity: 0,
+    width: '200vw',
+    height: '100vh',
     zIndex: 1,
-    opacity: 0
+    transform: 'translate(-20%,0)',
+    overflow: 'hidden'
+  },
+  video: {
+    position: 'relative',
+    zIndex: 2
   }
 }
 
 export default class LandingPageInformation extends React.Component {
-  componentDidMount(){
-    $('h1').animate({
-      opacity: 1
+  componentDidMount() {
+    $('#videoDiv').animate({
+      opacity: 0.75
     }, 1000)
   }
 
-  render(){
-    return <h1 style={styles.h1}>Information should go here.</h1>
+  render() {
+    return <div style={styles.div}>
+      <div id='videoDiv' style={styles.videoDiv}>
+      </div>
+
+      <iframe style={styles.video} width="1280" height="720" src="https://www.youtube.com/embed/IFfLCuHSZ-U"
+              frameborder="0" allowfullscreen></iframe>
+    </div>
   }
 }
