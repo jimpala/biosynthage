1bd664d98207a03f84b02d4e91e45bdd99af9fe2
James Pyne <james.pyne.13@ucl.ac.uk>
2016-07-15 10:23:00 +0100

    Navbar under construction but encountered sizing issues with MainSlide.

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -15,76 +15,76 @@ import MainSlide from './MainSlide.jsx';
 import LandingPageInformation from './LandingPageInformation.jsx';
 
 const styles = {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
 }
 
 export default class LandingPage extends React.Component {
 
     // Appends state calibration to standard Component constructor.
 
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      displaySlide: 0
-    };
-  }
+        this.state = {
+            displaySlide: 0
+        };
+    }
 
-  /*
-  learnMore()
-  -> Fade out logo on front.
-  -> Change displaySlide state to 1 upon finish.
-   */
-  learnMore() {
-    $('#mainSlide').animate({
-      opacity: 0
-    }, 1000, ()=> {
-      this.setState({
-        displaySlide: 1
-      });
-    })
-  }
+    /*
+     learnMore()
+     -> Fade out logo on front.
+     -> Change displaySlide state to 1 upon finish.
+     */
+    learnMore() {
+        $('#mainSlide').animate({
+            opacity: 0
+        }, 1000, ()=> {
+            this.setState({
+                displaySlide: 1
+            });
+        })
+    }
 
-  /*
-  learnLess()
-  -> Fade out landing video; fade in logo.
-  -> Change displaySlide state to 0 upon finish.
-   */
-  learnLess() {
-    $('#mainSlide').animate({
-      opacity: 1
-    }, 1000);
+    /*
+     learnLess()
+     -> Fade out landing video; fade in logo.
+     -> Change displaySlide state to 0 upon finish.
+     */
+    learnLess() {
+        $('#mainSlide').animate({
+            opacity: 1
+        }, 1000);
 
-    $('#landingVideo').animate({
-      opacity: 0
-    }, 1000);
+        $('#landingVideo').animate({
+            opacity: 0
+        }, 1000);
 
-    $('#videoDiv').animate({
-      opacity: 0
-    }, 1000, ()=> {
-      this.setState({
-        displaySlide: 0
-      });
-    })
+        $('#videoDiv').animate({
+            opacity: 0
+        }, 1000, ()=> {
+            this.setState({
+                displaySlide: 0
+            });
+        })
 
-  }
+    }
 
-  /*
-  render()
+    /*
+     render()
 
-   */
-  render() {
-    return (
-      <div className="landing-main-slide">
-        {this.state.displaySlide == 0 ?
-          <MainSlide learnMore={this.learnMore.bind(this)}/> :
-          <LandingPageInformation learnLess={this.learnLess.bind(this)}/>}
-      </div>
-    )
-  }
+     */
+    render() {
+        return (
+            <div className="landing-main-slide">
+                {this.state.displaySlide == 0 ?
+                    <MainSlide learnMore={this.learnMore.bind(this)}/> :
+                    <LandingPageInformation learnLess={this.learnLess.bind(this)}/>}
+            </div>
+        )
+    }
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -9,42 +9,86 @@
 
 import React from 'react';
 
-const styles = {
-  menu: {
-    position: 'fixed',
-    zIndex: 3,
-    backgroundColor: 'white',
-    width: '100vw'
-  }
 
-}
+// export default class Menu extends React.Component {
+//   render(){
+//     return (
+//       <div className="container" style={styles.menu}>
+//       <div className="navbar-header">
+//         <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+//           <span className="sr-only">Toggle navigation</span>
+//           <span className="icon-bar"></span>
+//           <span className="icon-bar"></span>
+//           <span className="icon-bar"></span>
+//         </button>
+//         <a className="navbar-brand" href="#">Project name</a>
+//       </div>
+//       <div id="navbar" className="navbar-collapse collapse">
+//         <ul className="nav navbar-nav">
+//           <li className="active"><a href="#">Home</a></li>
+//           <li><a href="#description">About</a></li>
+//           <li><a href="#contact">Contact</a></li>
+//         </ul>
+//         <ul className="nav navbar-nav navbar-right">
+//           <li><a href="../navbar/">Default</a></li>
+//           <li><a href="../navbar-static-top/">Static top</a></li>
+//           <li className="active"><a href="./">Fixed top <span className="sr-only">(current)</span></a></li>
+//         </ul>
+//       </div>
+//     </div>
+//     )
+//   }
+// }
+
+const styles = {
+    header: {
+        display: 'flex',
+        position: 'fixed',
+        zIndex: 3,
+        backgroundColor: 'rgba(0, 66, 119, 0.8)',
+        borderBottom: '1px solid #DADFE1',
+        borderBottomColor: 'rgba(255, 255, 255, 0.8)',
+        opacity: 0.8,
+        height: '44px',
+        width: '100vw',
+        paddingTop: '25px',
+        paddingBottom: '25px',
+    },
+    container: {
+
+    },
+    headerContent: {
+
+    },
+    navLogo: {
+
+    },
+    navLogoLink: {
+
+    },
+    navLogoInner: {
+
+    },
+};
 
 export default class Menu extends React.Component {
-  render(){
-    return (
-      <div className="container" style={styles.menu}>
-      <div className="navbar-header">
-        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-          <span className="sr-only">Toggle navigation</span>
-          <span className="icon-bar"></span>
-          <span className="icon-bar"></span>
-          <span className="icon-bar"></span>
-        </button>
-        <a className="navbar-brand" href="#">Project name</a>
-      </div>
-      <div id="navbar" className="navbar-collapse collapse">
-        <ul className="nav navbar-nav">
-          <li className="active"><a href="#">Home</a></li>
-          <li><a href="#description">About</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-        <ul className="nav navbar-nav navbar-right">
-          <li><a href="../navbar/">Default</a></li>
-          <li><a href="../navbar-static-top/">Static top</a></li>
-          <li className="active"><a href="./">Fixed top <span className="sr-only">(current)</span></a></li>
-        </ul>
-      </div>
-    </div>
-    )
-  }
+    render() {
+        return (
+            <header className="nav" style={styles.header}>
+                <div className="container" style={styles.container}>
+                    <div style={styles.headerContent}>
+                        <div style={styles.navLogo}>
+                            <a href="/" style={styles.navLogoLink}>
+                                <span style={styles.navLogoInner}>
+                                    <svg>
+
+                                    </svg>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        )
+    }
 }
