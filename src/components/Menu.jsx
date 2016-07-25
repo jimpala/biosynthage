fff7798d5145f43318743e61f043b52948e5984e
James Pyne <james.pyne.13@ucl.ac.uk>
2016-07-25 15:16:51 +0100

    Nav logo text amended to taste, with background for legibility.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -12,9 +12,7 @@ import {Link} from 'react-router';
 
 const styles = {
     navLink: {
-        borderTop: 'whitesmoke solid 4px',
-        fontWeight: '700',
-        opacity: '1',
+
     },
 };
 
@@ -41,14 +39,17 @@ export default class Menu extends React.Component {
                     <div className="logoWrap">
                         <img className="logo" id="igem" src="http://biosynthage.appspot.com/img/IgemLogoGreyIcon.png"
                              alt=""/>
-                        <img className="logo" id="biosynth" src="http://biosynthage.appspot.com/img/Ucligem_icon.png" alt=""/>
+                        <img className="logo" id="biosynth" src="http://biosynthage.appspot.com/img/Ucligem_icon.png"
+                             alt=""/>
                         <div id="logoTextBed">
-                            <h2 className="logoText">UCL iGEM</h2>
-                            <h2 className="logoText">Team 2016</h2>
+                            <div id="logoTextBG">
+                                <h2 className="logoText">UCL iGEM</h2>
+                                <h2 className="logoText">Team 2016</h2>
+                            </div>
                         </div>
                     </div>
                     <ul className="navOptionsList">
-                        <li className="navOptions"><Link to="/about" activeStyle={styles.navLink}>About</Link></li>
+                        <li className="navOptions"><Link to="/" activeStyle={styles.navLink}>About</Link></li>
                         <li className="navOptions">Team</li>
                         <li className="navOptions">Contact</li>
                     </ul>
